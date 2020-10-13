223d0552798ddd16828106e4cc0932d05177c8af
Jordan Craig <jordancraig1812@gmail.com>
2020-10-13 09:13:01 +0800

    Refactor

--- Cell.js
+++ Cell.js
@@ -6,7 +6,7 @@ function Cell(x, y, z, size) {
     this.z = z * size;
     this.size = size;
     this.neighbourCount = 0;
-    if(random(1) < 0.05) {
+    if(random(1) < percentBombs) {
         this.bomb = true;
     } else {
         this.bomb = false;
@@ -27,10 +27,8 @@ Cell.prototype.show = function(selected) {
 
     if(this.revealed) {
         if(this.bomb) {
-            //translate(this.size/2, this.size/2, this.size/2);
             fill('rgba(255, 33, 0, 0.5)');
             sphere(11, 10, 10);
-            //translate(-(this.size/2), -(this.size/2), -(this.size/2));
         } else {
             if(this.neighbourCount > 0) {
                 can.background('rgba(251, 0, 133, 0.7)');
@@ -62,9 +60,9 @@ Cell.prototype.floodFill = function() {
                 let posX = this.index[0]+x;
                 let posY = this.index[1]+y;
                 let posZ = this.index[2]+z;
-                if(posX > -1 && posX < 10) {
-                    if(posY > -1 && posY < 10) {
-                        if(posZ > -1 && posZ < 10) {
+                if(posX > -1 && posX < cubeAxisLength) {
+                    if(posY > -1 && posY < cubeAxisLength) {
+                        if(posZ > -1 && posZ < cubeAxisLength) {
                             let neighbour = map[posX][posY][posZ];
                             if(!neighbour.bomb && !neighbour.revealed) {
                                 neighbour.hit();
@@ -90,9 +88,9 @@ Cell.prototype.countBombs = function() {
                 let posX = this.index[0]+x;
                 let posY = this.index[1]+y;
                 let posZ = this.index[2]+z;
-                if(posX > -1 && posX < 10) {
-                    if(posY > -1 && posY < 10) {
-                        if(posZ > -1 && posZ < 10) {
+                if(posX > -1 && posX < cubeAxisLength) {
+                    if(posY > -1 && posY < cubeAxisLength) {
+                        if(posZ > -1 && posZ < cubeAxisLength) {
                             let neighbour = map[posX][posY][posZ];
                             if(neighbour.bomb) {
                                 total++;
@@ -105,73 +103,4 @@ Cell.prototype.countBombs = function() {
     }
 
     this.neighbourCount = total;
-}
-
-// Creates 3d vectors for the top left and bottom right of the cube (defining a bounding box)
-// Creates 3d vectors for the start and end of the line segment
-// Checks for a hit and returns a 3d vector representing the location of the hit in 3d space
-// Gets the length of the line from the start position to the hit position
-// If there is a hit, return the absolute length of this line, if not return -1
-Cell.prototype.checkHit = function(camX, camY, camZ, centX, centY, centZ) {
-    if(this.revealed) return -1;
-    let b1 = glMatrix.vec3.fromValues(this.x, this.y, this.z);
-    let b2 = glMatrix.vec3.fromValues(this.x+this.size, this.y+this.size, this.z+this.size);
-    let l1 = glMatrix.vec3.fromValues(camX, camY, camZ);
-    let l2 = glMatrix.vec3.fromValues(centX, centY, centZ);
-
-    let hit = glMatrix.vec3.create();
-
-    let isHit = checkLineBox(b1, b2, l1, l2, hit);
-
-    let len = glMatrix.vec3.create();
-    glMatrix.vec3.subtract(len, l1, hit); // Get the difference between origin and intersection points
-
-    return isHit ? Math.abs(glMatrix.vec3.length(len)) : -1;
-}
-
-// all args are Vec3, Hit will be filled by this algo
-function checkLineBox( B1, B2, L1, L2, Hit) {
-    if (L2[0] < B1[0] && L1[0] < B1[0]) return false;
-    if (L2[0] > B2[0] && L1[0] > B2[0]) return false;
-    if (L2[1] < B1[1] && L1[1] < B1[1]) return false;
-    if (L2[1] > B2[1] && L1[1] > B2[1]) return false;
-    if (L2[2] < B1[2] && L1[2] < B1[2]) return false;
-    if (L2[2] > B2[2] && L1[2] > B2[2]) return false;
-    if (L1[0] > B1[0] && L1[0] < B2[0] &&
-        L1[1] > B1[1] && L1[1] < B2[1] &&
-        L1[2] > B1[2] && L1[2] < B2[2])
-    {
-        glMatrix.vec3.set( Hit, L1);
-        return true;
-    }
-
-    if ((getIntersection(L1[0] - B1[0], L2[0] - B1[0], L1, L2, Hit) && inBox(Hit, B1, B2, 1))
-    || (getIntersection(L1[1] - B1[1], L2[1] - B1[1], L1, L2, Hit) && inBox(Hit, B1, B2, 2))
-    || (getIntersection(L1[2] - B1[2], L2[2] - B1[2], L1, L2, Hit) && inBox(Hit, B1, B2, 3))
-    || (getIntersection(L1[0] - B2[0], L2[0] - B2[0], L1, L2, Hit) && inBox(Hit, B1, B2, 1))
-    || (getIntersection(L1[1] - B2[1], L2[1] - B2[1], L1, L2, Hit) && inBox(Hit, B1, B2, 2))
-    || (getIntersection(L1[2] - B2[2], L2[2] - B2[2], L1, L2, Hit) && inBox(Hit, B1, B2, 3)))
-        return true;
-
-    return false;
-}
-
-function getIntersection( fDst1, fDst2, P1, P2, Hit) {
-    let temp = glMatrix.vec3.create();
-
-    if ((fDst1 * fDst2) >= 0) return false;
-    if (fDst1 == fDst2) return false;
-
-    glMatrix.vec3.subtract(temp, P2, P1);
-    glMatrix.vec3.scale( temp, temp, (-fDst1 / (fDst2 - fDst1)));
-    glMatrix.vec3.add( Hit, temp, P1);
-
-    return true;
-}
-
-function inBox(Hit, B1, B2, Axis) {
-    if (Axis == 1 && Hit[2] > B1[2] && Hit[2] < B2[2] && Hit[1] > B1[1] && Hit[1] < B2[1]) return true;
-    if (Axis == 2 && Hit[2] > B1[2] && Hit[2] < B2[2] && Hit[0] > B1[0] && Hit[0] < B2[0]) return true;
-    if (Axis == 3 && Hit[0] > B1[0] && Hit[0] < B2[0] && Hit[1] > B1[1] && Hit[1] < B2[1]) return true;
-    return false;
 }
--- index.js
+++ index.js
@@ -11,6 +11,9 @@ function make3DArray(x, y, z) {
     return arr;
 }
 
+
+let percentBombs = 0.05; // Between 0 and 1
+let cubeAxisLength = 10; // Number of cubes along each axis
 let map;
 let size = 20;
 let can;
@@ -19,7 +22,7 @@ let selection = [0, 0, 9];
 function setup() {
     createCanvas(600, 600, WEBGL);
     can = createGraphics(size, size, P2D);
-    map = make3DArray(10, 10, 10);
+    map = make3DArray(cubeAxisLength, cubeAxisLength, cubeAxisLength);
     // Init map
     for(let x=0; x<map.length; x++) {
         for(let y=0; y<map[x].length; y++) {
@@ -38,13 +41,9 @@ function setup() {
     }
 }
 
-let rays = [];
 function draw() {
-    /*console.log(this._renderer._curCamera.eyeX,
-        this._renderer._curCamera.eyeZ);*/
     background(0);
     orbitControl();
-    //translate(-100, -100);
 
     // Render map
     for(let x=0; x<map.length; x++) {
@@ -60,56 +59,7 @@ function draw() {
             }
         }
     }
-    //drawCrosshair();
-    //image(can, 0, 0);
-
-    /*for(let i=0; i<rays.length; i++) {
-        rays[i].show();
-    }*/
 }
-
-// Iterates over every cube and calls checkHit on each one
-// Finds the shortest length to a hit cube
-// Returns the 3d array values of that cube or null if no cube is hit
-/*
-function raytrace() {
-    let closestHit = null;
-    let closestLength = null;
-    for(let x=0; x<map.length; x++) {
-        for(let y=0; y<map[x].length; y++) {
-            for(let z=0; z<map[x][y].length; z++) {
-                let hitLength = map[x][y][z].checkHit(this._renderer._curCamera.eyeX, 
-                    this._renderer._curCamera.eyeY, 
-                    this._renderer._curCamera.eyeZ,
-                    this._renderer._curCamera.centerX,
-                    this._renderer._curCamera.centerY,
-                    this._renderer._curCamera.centerZ);
-                if(hitLength === 0) return [x, y, z];
-                if(hitLength > 0) {
-                    if(!closestLength) {
-                        closestLength = hitLength;
-                        closestHit = [x, y, z];
-                    } else if(hitLength < closestLength) {
-                        closestLength = hitLength;
-                        closestHit = [x, y, z];
-                    }
-                }
-            }
-        }
-    }
-    console.log(closestHit);
-    return closestHit;
-}
-*/
-/*
-function drawCrosshair() {
-    can.stroke('rgba(255, 0, 0, 1)');
-    can.line((width/2), (height/2)-10, 
-        (width/2), (height/2)+10);
-    can.line((width/2)-10, (height/2), 
-        (width/2)+10, (height/2));
-}
-*/
 
 function keyPressed() {
     if(keyCode === 32) {
@@ -117,14 +67,6 @@ function keyPressed() {
         if(map[selection[0]][selection[1]][selection[2]].bomb) {
             gameOver();
         }
-        //let hitCube = raytrace();
-        //if(hitCube) map[hitCube[0]][hitCube[1]][hitCube[2]].hit();
-        /*renderRay(this._renderer._curCamera.eyeX,
-            this._renderer._curCamera.eyeY, 
-            this._renderer._curCamera.eyeZ,
-            this._renderer._curCamera.centerX,
-            this._renderer._curCamera.centerY,
-            this._renderer._curCamera.centerZ);*/
     }
     if(keyCode === UP_ARROW) up();
     if(keyCode === DOWN_ARROW) down();
@@ -132,43 +74,36 @@ function keyPressed() {
     if(keyCode === RIGHT_ARROW) right();
     if(keyCode === 190) moveIn();
     if(keyCode === 188) moveOut();
-    console.log(keyCode);
 }
 
 function down() {
     ++selection[1];
-    if(selection[1] > 9) selection[1] = 0;
-    //if(map[selection[0]][selection[1]][selection[2]].revealed) down();
+    if(selection[1] > (cubeAxisLength-1)) selection[1] = 0;
 }
 
 function up() {
     --selection[1];
-    if(selection[1] < 0) selection[1] = 9;
-    //if(map[selection[0]][selection[1]][selection[2]].revealed) up();
+    if(selection[1] < 0) selection[1] = (cubeAxisLength-1);
 }
 
 function left() {
     --selection[0];
-    if(selection[0] < 0) selection[0] = 9;
-    //if(map[selection[0]][selection[1]][selection[2]].revealed) left();
+    if(selection[0] < 0) selection[0] = (cubeAxisLength-1);
 }
 
 function right() {
     ++selection[0];
-    if(selection[0] > 9) selection[0] = 0;
-    //if(map[selection[0]][selection[1]][selection[2]].revealed) right();
+    if(selection[0] > (cubeAxisLength-1)) selection[0] = 0;
 }
 
 function moveIn() {
     ++selection[2];
-    if(selection[2] > 9) selection[2] = 0;
-    //if(map[selection[0]][selection[1]][selection[2]].revealed) right();
+    if(selection[2] > (cubeAxisLength-1)) selection[2] = 0;
 }
 
 function moveOut() {
     --selection[2];
-    if(selection[2] < 0) selection[2] = 9;
-    //if(map[selection[0]][selection[1]][selection[2]].revealed) left();
+    if(selection[2] < 0) selection[2] = (cubeAxisLength-1);
 }
 
 function gameOver() {
@@ -180,24 +115,3 @@ function gameOver() {
         }
     }
 }
-
-/*
-function renderRay(eyeX, eyeY, eyeZ, x, y, z) {
-    let obj = {
-        x: eyeX,
-        y: eyeY, 
-        z: eyeZ,
-        cx: x,
-        cy: y,
-        cz: z
-    };
-    obj.deltaX = obj.x-obj.cx,
-    obj.deltaY = obj.y-obj.cy,
-    obj.deltaZ = obj.z-obj.cz,
-    obj.show = function() {
-        translate(obj.x, obj.y, obj.z);
-        box(obj.deltaX, obj.deltaY, obj.deltaZ);
-        translate(-obj.x, -obj.y, -obj.z);
-    }
-    rays.push(obj);
-}*/
